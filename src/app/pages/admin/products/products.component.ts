4857ab344fe769c40279d2966a2e43a33fd06f9f
IT_TEK_VITU\Nssf_User <tujames77@gmail.com>
2025-10-08 16:05:36 +0700

    update

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule, FormsModule ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  isSidePanalVisible: boolean = false;
+  productObj: any = {
+  "productId": 0,
+  "productSku": "",
+  "productName": "",
+  "productPrice": 0,
+  "productShortName": "",
+  "productDescription": "",
+  "createdDate": new Date(),
+  "deliveryTimeSpan": "",
+  "categoryId": 2147483647,
+  "productImageUrl": "",
+  "userId": 0
+  }
 
+  categoryList: any[] = [];
+
+  constructor( private productSrv: ProductService) {
+    
+  }
+
+  ngOnInit(): void{ }
+  
+  getAllCategory() {
+    this.productSrv.getCategory().subscribe((res: any) => {
+      this.categoryList = res.data;
+    });
+  }
+
+  //Method (Function inside a Class) when click new it open to create 
+  openSidePanal() {
+    this.isSidePanalVisible = true;
+  }
+  // when click close it close the new product form
+  closeSidePanal() {
+    this.isSidePanalVisible = false;
+  }
 }
---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Constant } from '../constant/constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+  
+  getAllProducts() {
+    return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_ALL_CATAGORY);
+  }
+
+  getCategory() {
+  return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_ALL_CATAGORY);
+}
+}
